19d6e832a0ffdd5cb41c145d1856033270bfa573
Inturi Kirthana <inturikirthana2808@gmail.com>
2026-06-12 22:16:02 +0530

    Use Render PostgreSQL

--- server.js
+++ server.js
@@ -3,12 +3,18 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
+
 const userRoutes = require("./routes/userRoutes");
 
 // Middleware
-app.use(cors());
-app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+    credentials: true
+  })
+);
 
+app.use(express.json());
 
 // Routes
 app.use("/api/auth", require("./routes/authRoutes"));
@@ -19,8 +25,6 @@ app.use("/api/credits", require("./routes/creditRoutes"));
 
 app.use("/api/membership", require("./routes/membershipRoutes"));
 
-
-// IMPORTANT CHANGE
 app.use("/api/match", require("./routes/matchRequestRoutes"));
 
 app.use("/api/profile", require("./routes/profileRoutes"));
@@ -33,23 +37,19 @@ app.use("/api/sessions", require("./routes/sessionRoutes"));
 
 app.use("/api/notifications", require("./routes/notificationRoutes"));
 
-app.use("/uploads", express.static("uploads"));
-
 app.use("/api/users", userRoutes);
+
+// Serve uploaded profile images
+app.use("/uploads", express.static("uploads"));
 
 // Test Route
 app.get("/", (req, res) => {
-
   res.send("🚀 Connect Via Skills Backend Running");
-
 });
-
 
 // Server
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-
   console.log(`🚀 Server running on port ${PORT}`);
-
 });
